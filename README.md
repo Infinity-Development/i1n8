bc52c3e429b98a1faccdddecafec8d06f2ec4308
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2022-03-18 21:37:35 -0600

    initial(commit): Add initial files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # i1n8
 Localization files for Infinity Bot List
+
+---
+
+## Contributing
+- Coming Soon
+
+---
---
+++ locales/en-US.js
@@ -0,0 +1,26 @@
+module.exports = {
+    /** Title Text */
+    "site_name": "Infinity Bot List",
+    /** NavBar Text */
+    "main_nav": {
+        "about": "About Us",
+        "discord": "Discord",
+        "all_bots": "Bots",
+        "bot_packs": "Packs",
+        "partners": "Partners",
+        "docs_site": "Docs",
+        "bot_queue": "Queue",
+        "ibl_status": "Queue",
+    },
+    /** User Nav Text */
+    "user_nav": {
+        "profile": "Profile",
+        "panel": "Panel",
+        "add_bot": "Add Bot",
+        "add_pack": "Add Pack",
+        "premium": "Premium",
+        "cert_bot": "Certification",
+        "ibl_stats": "Stats",
+        "news_title": "News",
+    },
+}
