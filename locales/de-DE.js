ddcd3ff119d45dcd963e93c9615fb3404cde3682
Pythonwolf7734 <82569523+Pythonwolf7734@users.noreply.github.com>
2022-03-19 20:24:29 +0100

    Create de-DE.js

---
+++ locales/de-DE.js
@@ -0,0 +1,26 @@
+module.exports = {
+    /** Title Text */
+    "site_name": "Infinity Bot List",
+    /** NavBar Text */
+    "main_nav": {
+        "about": "Über uns",
+        "discord": "Discord",
+        "all_bots": "Bots",
+        "bot_packs": "Packs",
+        "partners": "Partner",
+        "docs_site": "Dokumentation",
+        "bot_queue": "Warteschlange",
+        "ibl_status": "Status",
+    },
+    /** User Nav Text */
+    "user_nav": {
+        "profile": "Profil",
+        "panel": "Panel",
+        "add_bot": "Bot hinzufügen",
+        "add_pack": "Pack hinzufügen",
+        "premium": "Premium",
+        "cert_bot": "Zertifizierung",
+        "ibl_stats": "Statistiken",
+        "news_title": "Neuigkeiten",
+    },
+}
